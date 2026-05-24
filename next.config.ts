8eacf2bc41bdc06a23ea2f377ac406a695001856
Oscar Aquino <nexuslabsrosario@gmail.com>
2026-05-24 12:13:45 -0300

    Configuración para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/nexuslabs-website',
+  assetPrefix: '/nexuslabs-website/',
 };
 
 export default nextConfig;
