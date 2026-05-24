62b3202d24fa4994110b9f363541cd447d0f0d11
Oscar Aquino <nexuslabsrosario@gmail.com>
2026-05-24 12:23:12 -0300

    Fix configuración para Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/nexuslabs-website',
-  assetPrefix: '/nexuslabs-website/',
 };
 
 export default nextConfig;
